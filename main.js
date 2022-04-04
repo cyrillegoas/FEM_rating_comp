f3677a5d5ab74aa135f2a77ae4b9e44d58d86c89
Cyril Le Goas <cyril.legoas@gmail.com>
2022-04-04 18:00:27 +0200

    ADD confirmation message

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 import './style.scss';
 
 function RatingModal(params) {
-  const app = document.querySelector('#app');
-  const rateMeBtn = app.querySelector('.btn');
-  this.askRating(app);
-  const dialog = app.querySelector('.rating');
+  this.app = document.querySelector('#app');
+  this.app.innerHTML = `<button class="btn">Open rating modal</button><dialog class="rating"></dialog>`;
+  const rateMeBtn = this.app.querySelector('.btn');
+  this.dialog = this.app.querySelector('.rating');
+  this.askRating(this.dialog);
 
   // EVENT LISTENERS
+  this.dialog.addEventListener('click', (event) => this.closeModal(event));
   rateMeBtn.addEventListener('click', () => {
-    dialog.showModal();
+    this.dialog.showModal();
   });
 }
 
 RatingModal.prototype.askRating = function (element) {
   const html = `
-    <dialog class="rating">
+    <div class="rating__inner-wrapper">
       <div class="rating__wrapper">
         <div class="rating__star-wrapper">
           <img class="rating__star" src="./assets/icon-star.svg">
@@ -35,13 +37,47 @@ RatingModal.prototype.askRating = function (element) {
           <button class="btn">SUBMIT</button>
         </form>
       </div>
-    </dialog>
+    </div>
   `;
-  element.insertAdjacentHTML('beforeend', html);
+  element.innerHTML = html;
+  this.dialog
+    .querySelector('form')
+    .addEventListener('submit', (event) => this.handleSubmit(event), {
+      once: true,
+    });
 };
 
-document.querySelector('#app').innerHTML = `
-  <button class="btn">Open rating modal</button>
-`;
+RatingModal.prototype.confirmMessage = function (element) {
+  const html = `
+    <div class="rating__inner-wrapper">
+      <div class="rating__confirmation">
+        <img class="rating__confirmation-img"src="./assets/illustration-thank-you.svg">
+        <p class="rating__confirmation-message">You selected ${this.userAnswer} out of 5</p>
+        <h1 class="rating__confirmation-title">How did we do?</h1>
+        <p class="rating__confirmation-desc">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+      </div>
+    </div>
+  `;
+  element.innerHTML = html;
+};
+
+RatingModal.prototype.handleSubmit = function (event) {
+  event.preventDefault();
+  const form = this.dialog.querySelector('form');
+  const checkedRadio = form.querySelector('input[name="rating"]:checked');
+  if (!checkedRadio) {
+    // nothing selected
+    return;
+  }
+  this.userAnswer = checkedRadio.value;
+  this.confirmMessage(this.dialog);
+};
+
+RatingModal.prototype.closeModal = function (event) {
+  if (event.target === event.currentTarget) {
+    this.dialog.close();
+    this.askRating(this.dialog);
+  }
+};
 
 const ratingModal = new RatingModal();
